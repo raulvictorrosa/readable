25d1b54de6fc23c519e6265fa09a234e0855ba8a
Raul Victor Rosa <raulvictorrosa@gmail.com>
2018-01-16 20:56:30 -0200

    feat: Add some parte of the code to the store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux'
+
+// import reducer from './reducers'
 
 import './styles/index.css';
 
 import Index from './components';
 import registerServiceWorker from './registerServiceWorker';
 
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
+)
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Index />
-  </BrowserRouter>,
+  <Provider store={store}>
+    {/* <BrowserRouter> */}
+      <Index />
+    {/* </BrowserRouter> */}
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
