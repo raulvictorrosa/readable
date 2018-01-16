14b69c60e3809465198da20ce37f4bfbd5495f70
Raul Victor Rosa <raulvictorrosa@gmail.com>
2018-01-16 20:58:55 -0200

    feat: Start the reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,55 @@
+import { combineReducers } from 'redux'
+
+import {
+  GET_POST,
+  // ADD_POST,
+  // DELETE_POST
+} from '../actions'
+
+function food(state = {}, action) {
+  switch (action.type) {
+    case ADD_POST:
+      const { recipe } = action
+
+      return {
+        ...state,
+        [recipe.label]: recipe
+      }
+
+    default:
+      return state
+  }
+}
+
+const initialPostState = {
+  id: null,
+  title: null,
+  body: null,
+  category: null
+}
+
+function posts(state = initialPostState, action) {
+  const { id, title, body, category } = action
+
+  switch (action.type) {
+    case ADD_POST:
+      const { post } = action
+      return {
+        ...state,
+        [id]: post
+        }
+      }
+    case DELETE_POST :
+      const { post } = action
+      return {
+        ...state,
+        [id]: post
+      }
+    default :
+      return state
+  }
+}
+
+export default combineReducers({
+  posts,
+})
