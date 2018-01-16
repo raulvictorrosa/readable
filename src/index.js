e19b4b5258bed465f3c57a2b3c9e2a2cd4c7b423
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-16 12:58:13 -0200

    feat: Comment some code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
 
 import './styles/index.css';
 
 import Index from './components';
 import registerServiceWorker from './registerServiceWorker';
+
+// const logger = store => next => action => {
+//   console.group(action.type)
+//   console.info('dispatching', action)
+//   let result = next(action)
+//   console.log('next state', store.getState())
+//   console.groupEnd(action.type)
+//   return result
+// }
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// const store = createStore(
+//   reducer,
+//   composeEnhancers(
+//     applyMiddleware(logger)
+//   )
+// )
 
 ReactDOM.render(
   <BrowserRouter>
