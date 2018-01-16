6e8ed54a3e77b7ff13158866ab41aed61ab073e0
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-16 17:49:52 -0200

    feat: Comment the line that shows timestramp in console

--- src/components/Post.js
+++ src/components/Post.js
@@ -52,7 +52,7 @@ class Post extends Component {
     const { post } = this.props;
     const { timestamp } = post
 
-    console.log(Moment().unix())
+    // console.log(Moment().unix())
     // addPost('asdasdasd', Date(), title, body, author, category)
 
     return (
