3996919f2ada11b237fdb92fa266b70c5f27f59c
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-19 16:40:51 -0200

    feat: Component Home in development

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
-import compose from 'recompose/compose';
-
-import * as actions from '../../actions/postActions';
-
 import Grid from 'material-ui/Grid';
-
 import Post from './Post'
 // import SinglePost from '../post/SinglePost'
-import * as actions from '../../actions/postActions';
+import * as actions from '../actions/postActions';
 
 class Home extends Component {
   static propTypes = {
     posts: PropTypes.array
-  }
-
-  state = {
-    posts: []
   }
 
   // componentDidMount() {
@@ -50,33 +41,41 @@ class Home extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  posts: state.postsById,
-  sortBy: state.setSorting ? state.setSorting.sort : ''
-})
+// const mapStateToProps = state => ({
+//   posts: state.postsById,
+//   sortBy: state.setSorting ? state.setSorting.sort : ''
+// })
 
-const mapDispatchToProps = dispatch => ({
-  dispatch,
-  fetchData: sortCriteria =>
-    dispatch(fetchPosts()).then(() => dispatch(setSorting(sortCriteria)))
-})
+// const mapDispatchToProps = dispatch => ({
+//   dispatch,
+//   fetchData: sortCriteria =>
+//     dispatch(fetchPosts()).then(() => dispatch(setSorting(sortCriteria)))
+// })
+
+// const mapStateToProps = ({ posts }, { match }) => ({
+//   const category = match.params.category
+//   return {
+//     posts: category ? posts.filter(post => post.category === category) : posts
+//   }
+// })
+function mapStateToProps({ posts }, { match }) {
+  const category = match.params.category
+  return {
+    posts: category ? posts.filter(post => post.category === category) : posts
+  }
+}
 
 // export default withStyles(styles, { withTheme: true })(Home);
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+// export default connect(mapStateToProps, mapDispatchToProps)(Home)
 
-export default compose(
-  withStyles(styles, {
-    name: 'AppFrame',
-  }),
-  withWidth(),
-  connect(),
-)(AppFrame);
+// export default compose(
+//   withStyles(styles, {
+//     withTheme: true,
+//   }),
+//   connect(
+//     mapStateToProps,
+//   ),
+// )(Home);
 
-export default compose(
-  withStyles(styles, {
-    name: 'AppFrame',
-  }),
-  connect(state => ({
-    uiTheme: state.theme,
-  })),
-)(AppFrame);
+
+export default connect(mapStateToProps, actions)(Home)
