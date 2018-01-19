981e15db8cbc334c728368921b1e60a9dffeddf3
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-19 16:39:23 -0200

    feat: Organize actions types

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,18 +1,26 @@
-//related to category
-export const FETCH_CATEGORY = 'FETCH_CATEGORY'
+/**
+* @description Action to fetch categories
+*/
+export const FETCH_CATEGORIES = 'FETCH_CATEGORIES'
 
-//related to comments
+/**
+* @description Actions related to posts
+*/
+export const FETCH_POSTS_BY_CATEGORY = 'FETCH_POSTS_BY_CATEGORY'
+export const FETCH_POSTS = 'FETCH_POSTS'
+export const ADD_POST = 'ADD_POST'
+export const FETCH_POST_BY_ID = 'FETCH_POST_BY_ID'
+export const VOTE_POST = 'VOTE_POST'
+export const EDIT_POST = 'EDIT_POST'
+export const DELETE_POST = 'DELETE_POST'
+export const SORT_POST = 'SORT_POST'
+
+/**
+* @description Actions related to comments
+*/
 export const FETCH_COMMENTS = 'FETCH_COMMENTS'
 export const ADD_COMMENT = 'ADD_COMMENT'
-export const UPDATE_COMMENT = 'UPDATE_COMMENT'
-export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const FETCH_COMMENT = 'FETCH_COMMENT'
 export const VOTE_COMMENT = 'VOTE_COMMENT'
-
-//related to posts
-export const FETCH_POSTS = 'FETCH_POSTS'
-export const GET_CATEGORY_POSTS = 'GET_CATEGORY_POSTS'
-export const ADD_POST = 'ADD_POST'
-export const UPDATE_POST = 'UPDATE_POST'
-export const DELETE_POST = 'DELETE_POST'
-export const VOTE_POST = 'VOTE_POST'
-export const SORT_POST = 'SORT_POST'
+export const EDIT_COMMENT = 'EDIT_COMMENT'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
