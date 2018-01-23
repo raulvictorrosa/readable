4979bf1f7959027d3b81f38a57e46a726eca3cb5
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-23 17:31:23 -0200

    BKP to be edited

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
 import Grid from 'material-ui/Grid';
-import Post from './Post'
+import Post from './posts/Post'
 // import SinglePost from '../post/SinglePost'
 import * as actions from '../actions/postActions';
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
-// import { Route } from 'react-router-dom';
-import { Link, Route, withRouter, Switch } from 'react-router-dom'
+import { Switch, Link, Route, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { sortPost } from '../actions/postActions'
-// import { fetchCategories } from '../actions/categoryActions'
+import { fetchCategories } from '../actions/categoryActions'
 import compose from 'recompose/compose';
 import Grid from 'material-ui/Grid';
 import { withStyles } from 'material-ui/styles';
 
 import Sidebar from './Sidebar';
 import Home from './Home'
-import PostNew from './PostNew'
+import PostNew from './posts/PostNew'
 
 const styles = theme => ({
   root: {
@@ -47,8 +46,14 @@ class Index extends Component {
     posts: PropTypes.array,
     categories: PropTypes.array
   }
+
+  componentDidMount() {
+    this.props.fetchCategories()
+  }
+
   render() {
-    const { classes } = this.props;
+    // const { classes } = this.props;
+    const { classes, categories, sortPost } = this.props;
 
     return (
       <div className={classes.root}>
@@ -57,10 +62,19 @@ class Index extends Component {
 
           <main className={classes.content}>
             <Grid container className={classes.root}>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/post-new' component={PostNew} />
               {/* <Route exact path='/:category' component={Category} />
               <Route path='/:category/:post_id' component={PostDetail} /> */}
+
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/post-new" component={PostNew} />
+                {/* <Route exact path="/:category" component={Category} /> */}
+                <Route exact path="/:category" component={Home} />
+                {/* <Route exact path="/:category/:postId" component={PostDetail} /> */}
+                {/* <Route path="/:category/:postId/edit" component={EditPost} /> */}
+                {/* <Route path="/:category/:postId/comment" component={NewComment} /> */}
+                {/* <Route path="/:category/:postId/:commentId/edit" component={EditComment} /> */}
+              </Switch>
             </Grid>
           </main>
         </div>
@@ -75,12 +89,23 @@ function mapStateToProps({ categories }) {
   }
 }
 
-// export default withStyles(styles, { withTheme: true })(Index)
-export default withRouter(
-  connect(
+export default compose(
+  withStyles(styles, {
+    withTheme: true,
+  }),
+  withRouter(connect(
     mapStateToProps, {
       sortPost,
-      // fetchCategories
+      fetchCategories
     }
-  )(Index)
-)
+  )),
+)(Index);
+
+// export default withStyles(styles, { withTheme: true })(Index)
+// export default withRouter(
+//   connect(
+//     mapStateToProps, {
+//     sortPost,
+//     fetchCategories
+//   })(Index)
+// )
