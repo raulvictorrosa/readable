a896cd06604ef62ef1cf5aa848cf3d04de122082
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-23 17:25:12 -0200

    feat : Api index

---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+import { fetchCategories } from './categories';
+import { fetchPostsByCategory, fetchPosts, addPost, fetchPostById, upvotePost, downvotePost, editPost, deletePost } from './posts';
+import { fetchComments, addComment, fetchComment, upvoteComment, downvoteComment, editComment, deleteComment } from './comments';
+
+export default {
+  fetchCategories,
+  fetchPostsByCategory,
+  fetchPosts,
+  addPost,
+  fetchPostById,
+  upvotePost,
+  downvotePost,
+  editPost,
+  deletePost,
+  fetchComments,
+  addComment,
+  fetchComment,
+  upvoteComment,
+  downvoteComment,
+  editComment,
+  deleteComment
+}
