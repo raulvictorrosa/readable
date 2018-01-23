66ec0a57f7395979b70a2943c7c84b7ed5568553
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-23 13:53:45 -0200

    feat: Enhance the code of post reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -8,21 +8,26 @@ import * as Types from '../actions/actionTypes'
 function posts(state = [], action) {
   const { posts, post, postId, updatedPost, sortKey } = action
   switch (action.type) {
+    case Types.FETCH_POSTS_BY_CATEGORY:
+      return posts.filter(post => !(post.deleted))
+
     case Types.FETCH_POSTS:
       return action.posts.filter(post => !(post.deleted))
-    case Types.GET_CATEGORY_POSTS:
-      return posts.filter(post => !(post.deleted))
+
     case Types.ADD_POST:
       return state.concat([post])
-    case Types.UPDATE_POST:
+
+    case Types.EDIT_POST:
       return state.map(post => {
         if (post.id === postId) {
           post = updatedPost
         }
         return post
       })
+
     case Types.DELETE_POST:
       return state.filter(post => post.id !== postId)
+
     case Types.VOTE_POST:
       return state.map(post => {
         if (post.id === action.postId) {
@@ -35,8 +40,10 @@ function posts(state = [], action) {
         }
         return post
       })
+
     case Types.SORT_POST:
       return [].concat(state.sort(sortBy("-" + sortKey)))
+
     default:
       return state
   }
