341dae0ea9fa2abcb898d2a746eb81b5d1cbeca2
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-24 17:36:51 -0200

    feat: Change the component to a stateless function component

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -18,43 +18,41 @@ const styles = theme => ({
   }
 });
 
-class MenuList extends Component {
-  static propTypes = {
+const MenuList = (props) => {
+  MenuList.propTypes = {
     classes: PropTypes.object.isRequired,
-  };
-
-  render() {
-    const { classes } = this.props
-
-    return (
-      <div>
-        <List>
-          <ListItem>
-            <Typography color="accent" type="title" noWrap>React Readble</Typography>
-          </ListItem>
-        </List>
-        <Divider />
-
-        <List className={classes.root}>
-          <ListItem button component={Link} to='/'>
-            <ListItemText primary="Posts" />
-          </ListItem>
-          <Collapse component="li" in>
-            <List disablePadding>
-              <ListItem button className={classes.nested} component={Link} to='/post-new'>
-                <ListItemIcon>
-                  <AddCircle />
-                </ListItemIcon>
-                <ListItemText primary="Add new" />
-              </ListItem>
-            </List>
-          </Collapse>
-
-          <Categories />
-        </List>
-      </div>
-    )
   }
+
+  const { classes } = props
+
+  return (
+    <div>
+      <List>
+        <ListItem>
+          <Typography color="accent" type="title" noWrap>React Readble</Typography>
+        </ListItem>
+      </List>
+      <Divider />
+
+      <List className={classes.root}>
+        <ListItem button component={Link} to='/'>
+          <ListItemText primary="Posts" />
+        </ListItem>
+        <Collapse component="li" in>
+          <List disablePadding>
+            <ListItem button className={classes.nested} component={Link} to='/post-new'>
+              <ListItemIcon>
+                <AddCircle />
+              </ListItemIcon>
+              <ListItemText primary="Add new" />
+            </ListItem>
+          </List>
+        </Collapse>
+
+        <Categories />
+      </List>
+    </div>
+  )
 }
 
 export default withStyles(styles)(MenuList);
