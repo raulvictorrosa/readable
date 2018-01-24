dedcc7c130cf75763737b96be72953dff4863b5d
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-24 17:11:08 -0200

    feat: Rename postsReducer.js to posts.js

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -2,16 +2,16 @@
 // Return one and the same result if the same arguments are passed in
 // Depend solely on the arguments passed into them
 // Do not produce side effects
-import sortBy from 'sort-by'
+// import sortBy from 'sort-by'
 import {
   FETCH_POSTS_BY_CATEGORY,
   FETCH_POSTS,
   ADD_POST,
   EDIT_POST,
   DELETE_POST,
-  VOTE_POST,
-  SORT_POST,
-} from '../actions/actionTypes'
+  // VOTE_POST,
+  // SORT_POST,
+} from '../actions'
 
 function posts(state = [], action) {
   const { posts, post, postId, updatedPost, sortKey } = action
@@ -36,21 +36,21 @@ function posts(state = [], action) {
     case DELETE_POST:
       return state.filter(post => post.id !== postId)
 
-    case VOTE_POST:
-      return state.map(post => {
-        if (post.id === action.postId) {
-          if (action.option === "upVote") {
-            post.voteScore += 1
-          }
-          if (action.option === "downVote") {
-            post.voteScore -= 1
-          }
-        }
-        return post
-      })
+    // case VOTE_POST:
+    //   return state.map(post => {
+    //     if (post.id === action.postId) {
+    //       if (action.option === "upVote") {
+    //         post.voteScore += 1
+    //       }
+    //       if (action.option === "downVote") {
+    //         post.voteScore -= 1
+    //       }
+    //     }
+    //     return post
+    //   })
 
-    case SORT_POST:
-      return [].concat(state.sort(sortBy("-" + sortKey)))
+    // case SORT_POST:
+    //   return [].concat(state.sort(sortBy("-" + sortKey)))
 
     default:
       return state
