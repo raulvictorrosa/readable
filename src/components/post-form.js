48b634a7116f1c1334c43e05add684bbd2f2d271
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-24 18:13:03 -0200

    feat: Archive post-form

---
+++ src/components/post-form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Grid from 'material-ui/Grid';
+
+class PostNew extends Component {
+  render() {
+    // const { classes } = this.props;
+
+    return (
+      <Grid item xs={12}>
+        <Grid container spacing={Number(24)}>
+
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+export default PostNew
