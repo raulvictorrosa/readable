045c25f6d03e277632f3c5badc79a2de104e6f5f
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-24 13:16:52 -0200

    feat: Active the logger with the thunk

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk'
 import registerServiceWorker from './registerServiceWorker';
-
-import './styles/index.css';
-
 import Index from './components';
 import reducer from './reducers'
 
+import './styles/index.css';
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-// const logger = store => next => action => {
-//   console.group(action.type)
-//   console.info('dispatching', action)
-//   let result = next(action)
-//   console.log('next state', store.getState())
-//   console.groupEnd(action.type)
-//   return result
-// }
-
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
 
 const store = createStore(
   reducer,
   composeEnhancers(
-    applyMiddleware(thunk)
-    // applyMiddleware(logger, thunk)
+    // applyMiddleware(thunk)
+    applyMiddleware(logger, thunk)
   )
 )
-
-// console.log(store.getState())
 
 ReactDOM.render(
   <Provider store={store}>
