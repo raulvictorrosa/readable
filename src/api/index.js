797ac9c3b50fa6bc1aa7bc19005f80838134edab
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-24 07:46:59 -0200

    feat: Comment exports not necessary for now

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,18 @@
 import { fetchCategories } from './categories';
-import { fetchPostsByCategory, fetchPosts, addPost, fetchPostById, upvotePost, downvotePost, editPost, deletePost } from './posts';
-import { fetchComments, addComment, fetchComment, upvoteComment, downvoteComment, editComment, deleteComment } from './comments';
+import { /* fetchPostsByCategory, */ fetchPosts, addPost, /* fetchPostById, */ upvotePost, downvotePost, editPost, deletePost } from './posts';
+import { /* fetchComments, */ addComment, fetchComment, upvoteComment, downvoteComment, editComment, deleteComment } from './comments';
 
 export default {
   fetchCategories,
-  fetchPostsByCategory,
+  // fetchPostsByCategory,
   fetchPosts,
   addPost,
-  fetchPostById,
+  // fetchPostById,
   upvotePost,
   downvotePost,
   editPost,
   deletePost,
-  fetchComments,
+  // fetchComments,
   addComment,
   fetchComment,
   upvoteComment,
