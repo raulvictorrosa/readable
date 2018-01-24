7016bd9525001fe3f2b78a76326df0bf980681e6
raulvictorrosa <raulvictorrosa@gmail.com>
2018-01-24 17:05:28 -0200

    fix: Some bugs fixed

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
-import { Switch, Link, Route, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { sortPost } from '../actions/postActions'
-import { fetchCategories } from '../actions/categoryActions'
+import { withRouter } from 'react-router-dom'
+// import { Switch, Link, Route, withRouter } from 'react-router-dom'
+import { PropTypes } from 'prop-types';
 import compose from 'recompose/compose';
-import Grid from 'material-ui/Grid';
-import { withStyles } from 'material-ui/styles';
 
-import Sidebar from './Sidebar';
-import Home from './Home'
-import PostNew from './posts/PostNew'
+// import { sortPost } from '../actions/posts'
+import { fetchCategories } from '../actions/categories'
+
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+
+import Header from './sidebar';
+import Sidebar from './header';
+// import Home from './Home'
+// import PostNew from './posts/PostNew'
 
 const styles = theme => ({
   root: {
@@ -43,21 +47,23 @@ const styles = theme => ({
 
 class Index extends Component {
   static propTypes = {
-    posts: PropTypes.array,
-    categories: PropTypes.array
+  //   posts: PropTypes.array,
+  //   categories: PropTypes.array,
+    classes: PropTypes.object.isRequired,
   }
 
   componentDidMount() {
+    // this.props.fetchPosts()
     this.props.fetchCategories()
   }
 
   render() {
-    // const { classes } = this.props;
-    const { classes, categories, sortPost } = this.props;
+    const { classes } = this.props;
 
     return (
       <div className={classes.root}>
         <div className={classes.appFrame}>
+          <Header />
           <Sidebar />
 
           <main className={classes.content}>
@@ -65,16 +71,16 @@ class Index extends Component {
               {/* <Route exact path='/:category' component={Category} />
               <Route path='/:category/:post_id' component={PostDetail} /> */}
 
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/post-new" component={PostNew} />
+              {/* <Switch>   */}
+                {/* <Route exact path="/" component={Home} /> */}
+                {/* <Route exact path="/post-new" component={PostNew} /> */}
                 {/* <Route exact path="/:category" component={Category} /> */}
-                <Route exact path="/:category" component={Home} />
+                {/* <Route exact path="/:category" component={Home} /> */}
                 {/* <Route exact path="/:category/:postId" component={PostDetail} /> */}
                 {/* <Route path="/:category/:postId/edit" component={EditPost} /> */}
                 {/* <Route path="/:category/:postId/comment" component={NewComment} /> */}
                 {/* <Route path="/:category/:postId/:commentId/edit" component={EditComment} /> */}
-              </Switch>
+              {/* </Switch>  */}
             </Grid>
           </main>
         </div>
@@ -83,29 +89,11 @@ class Index extends Component {
   }
 }
 
-function mapStateToProps({ categories }) {
-  return {
-    categories: categories
-  }
-}
-
 export default compose(
-  withStyles(styles, {
-    withTheme: true,
+  withStyles(styles),
+  withRouter,
+  connect(undefined, {
+    // fetchPosts,
+    fetchCategories
   }),
-  withRouter(connect(
-    mapStateToProps, {
-      sortPost,
-      fetchCategories
-    }
-  )),
 )(Index);
-
-// export default withStyles(styles, { withTheme: true })(Index)
-// export default withRouter(
-//   connect(
-//     mapStateToProps, {
-//     sortPost,
-//     fetchCategories
-//   })(Index)
-// )
