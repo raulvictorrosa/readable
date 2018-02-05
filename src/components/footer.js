27ed7e13eb1135996c458978b9274fc41e66345c
raulvictorrosa <raulvictorrosa@gmail.com>
2018-02-05 14:46:00 -0200

    feat: footer component

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <p className='footer'>
+    Created by Daniel Lee for Udacity React Nanodegree - Readable Project
+  </p>
+);
+
+export default Footer;
