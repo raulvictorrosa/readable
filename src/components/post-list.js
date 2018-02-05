ff1d02f0c68cde42c4c5a8e4007dac3195cbc30c
raulvictorrosa <raulvictorrosa@gmail.com>
2018-02-05 14:47:44 -0200

    feat: post list component

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -1,81 +1,31 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux'
-import Grid from 'material-ui/Grid';
-import Post from './post-card'
-// import SinglePost from '../post/SinglePost'
-import * as actions from '../actions/posts';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-class Home extends Component {
-  static propTypes = {
-    posts: PropTypes.array
-  }
+import { CONTENT_POSTS } from '../actions/constants';
+import Sorter from './sorter';
+import PostSummary from './post-summary';
 
-  // componentDidMount() {
-  //   ReadbleAPI.getPosts().then((posts) => {
-  //     this.setState({ posts })
-  //   })
-  // }
+const conditionalRender = (WrappedComponent) => ({ posts, ...props }) => (
+  posts.length === 0 ?
+  <WrappedComponent>
+    <p><em>Whoops! There are no posts to display!</em></p>
+  </WrappedComponent>
+  :
+  <WrappedComponent {...props}>
+    {posts.map(
+      post => <PostSummary key={post.id} {...post} />
+    )}
+  </WrappedComponent>
+)
 
-  componentWillMount() {
-    this.props.fetchPosts()
-  }
+const PostList = ({ children }) => (
+  <div className='post-list'>
+    <div className='post-submenu'>
+      <Link to='/posts/new'>New Post</Link>
+      <Sorter content={CONTENT_POSTS} />
+    </div>
+    {children}
+  </div>
+)
 
-  render() {
-    const { posts } = this.props
-    console.log(posts)
-
-    return (
-      <Grid item xs={12}>
-        <Grid container spacing={24}>
-          {posts.map(post => (
-            <Grid key={post.id} item xs={4}>
-              {/* <Post
-                post={post}
-              /> */}
-            </Grid>
-          ))}
-        </Grid>
-      </Grid>
-    )
-  }
-}
-
-// const mapStateToProps = state => ({
-//   posts: state.postsById,
-//   sortBy: state.setSorting ? state.setSorting.sort : ''
-// })
-
-// const mapDispatchToProps = dispatch => ({
-//   dispatch,
-//   fetchData: sortCriteria =>
-//     dispatch(fetchPosts()).then(() => dispatch(setSorting(sortCriteria)))
-// })
-
-// const mapStateToProps = ({ posts }, { match }) => ({
-//   const category = match.params.category
-//   return {
-//     posts: category ? posts.filter(post => post.category === category) : posts
-//   }
-// })
-function mapStateToProps({ posts }, { match }) {
-  const category = match.params.category
-  return {
-    posts: category ? posts.filter(post => post.category === category) : posts
-  }
-}
-
-// export default withStyles(styles, { withTheme: true })(Home);
-// export default connect(mapStateToProps, mapDispatchToProps)(Home)
-
-// export default compose(
-//   withStyles(styles, {
-//     withTheme: true,
-//   }),
-//   connect(
-//     mapStateToProps,
-//   ),
-// )(Home);
-
-
-export default connect(mapStateToProps, actions)(Home)
+export default conditionalRender(PostList);
