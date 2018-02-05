6102f9460d36d45463a51263337918d3a9aeae69
raulvictorrosa <raulvictorrosa@gmail.com>
2018-02-05 14:47:34 -0200

    feat: post form component

--- src/components/post-form.js
+++ src/components/post-form.js
@@ -1,19 +1,87 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
 
-import Grid from 'material-ui/Grid';
+import { capitalizeFirst } from '../utils/helper';
+import { createPost, editPost } from '../actions/posts';
 
-class PostNew extends Component {
-  render() {
-    // const { classes } = this.props;
-
-    return (
-      <Grid item xs={12}>
-        <Grid container spacing={Number(24)}>
-
-        </Grid>
-      </Grid>
-    )
-  }
+const PostForm = (props) => {
+  const { handleSubmit, pristine, submitting, categories,
+    createPost, editPost, history, match } = props;
+  const isEdit = match.url.indexOf('edit') !== -1;
+  return (
+    <form className='form' onSubmit={handleSubmit(data => {
+      const { title, body, category = categories[0].name, author } = data;
+      data = { title, body, category, author };
+      if(isEdit) {
+        editPost(match.params.id, data);
+      } else {
+        createPost(data);
+      }
+      history.goBack();
+      })}
+    >
+      <div className='form-field'>
+        <label>Title</label>
+        <div className='form-field-input'>
+          <Field
+            name='title'
+            component='input'
+            type='text'
+            placeholder='Super duper awesome title'
+          />
+        </div>
+      </div>
+      <div className='form-field'>
+        <label>Author</label>
+        <div className='form-field-input'>
+          <Field
+            name='author'
+            component='input'
+            type='text'
+            placeholder='Name that is going to be famous!'
+          />
+        </div>
+      </div>
+      <div className='form-field'>
+        <label>Category</label>
+        <div className='form-field-input'>
+          <Field name='category' component='select'>
+            {categories.map((category, index) =>
+              <option key={index} value={category.name}>{capitalizeFirst(category.name)}</option>
+            )}
+          </Field>
+        </div>
+      </div>
+      <div className='form-field'>
+        <label>Body</label>
+        <div className='form-field-input'>
+          <Field
+            name='body'
+            component='textarea'
+            placeholder='Write your life story here'
+          />
+        </div>
+      </div>
+      <div className='form-field'>
+        <button type='button' onClick={() => history.goBack()}>
+          Back
+        </button>
+        <button type='submit' disabled={pristine || submitting}>
+          Submit
+        </button>
+      </div>
+    </form>
+  )
 }
 
-export default PostNew
+const mapStateToProps = ({ categories }) => ({
+  categories
+})
+
+export default reduxForm({
+  form: 'post'
+})(withRouter(connect(mapStateToProps,
+  { createPost, editPost }
+)(PostForm)));
