f40446b249e04676efcb93792115d6d3fdc204b9
raulvictorrosa <raulvictorrosa@gmail.com>
2018-03-19 14:51:59 -0300

    feat: Improve the constants of the API

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,10 +1,17 @@
+export const BASE_URL = 'http://localhost:3001';
+
+// Generate a unique token for storing your readble data on the backend server.
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+export const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
 // magic keywords used for backend server request
 export const OPTION_UPVOTE = 'upVote';
 export const OPTION_DOWNVOTE = 'downVote';
 
-// api template
-export const BASE_URL='http://localhost:3001';
-export const HEADERS = {
-  'Authorization': 'some-auth-token',
-  'Content-Type': 'application/json'
-}
+
