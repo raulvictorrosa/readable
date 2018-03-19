f057a6bab18b29772599c5680d0995ee52c43d42
raulvictorrosa <raulvictorrosa@gmail.com>
2018-03-19 14:59:59 -0300

    feat: Improve component categories

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,15 +1,71 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { Link, withRouter } from 'react-router-dom'
 import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux'
+import { getCategories } from '../actions/Categories';
+import { fetchCategories } from '../api/Categories'
+import { Link, withRouter } from 'react-router-dom'
 import compose from 'recompose/compose';
 import { withStyles } from 'material-ui/styles';
 import List, { ListItem, ListItemText } from 'material-ui/List';
 import Collapse from 'material-ui/transitions/Collapse';
 import ExpandLess from 'material-ui-icons/ExpandLess';
 import ExpandMore from 'material-ui-icons/ExpandMore';
-// import { capitalize } from '../utils/helper';
-import { capitalizeFirst } from '../utils/helper';
+import { capitalize } from '../utils/helper';
+
+class Categories extends Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    categories: PropTypes.array.isRequired,
+  }
+
+  state = { open: true }
+
+  componentDidMount() {
+    const { dispatch } = this.props
+    fetchCategories().then((categories) => dispatch(getCategories(categories)))
+  }
+
+  handleClick = () => {
+    this.setState({ open: !this.state.open })
+  }
+
+  render() {
+    const { classes, categories, location } = this.props
+
+    if (categories.length > 0) {
+      return (
+        <div>
+          <ListItem button onClick={this.handleClick}>
+            <ListItemText primary="Categories" />
+            {this.state.open ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+
+          <Collapse component="li" in={this.state.open} timeout="auto" unmountOnExit>
+            <List disablePadding>
+              {categories.map((category, index) => (
+                location.pathname === `/${category.path}`
+                  ? <ListItem button key={category.path} className={classes.nested}>
+                    <ListItemText primary={capitalize(category.name)} />
+                  </ListItem>
+                  : <ListItem button key={category.path} component={Link} to={`/${category.path}`} className={classes.nested}>
+                    <ListItemText primary={capitalize(category.name)} />
+                  </ListItem>
+              ))}
+            </List>
+          </Collapse>
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <ListItem button onClick={this.handleClick}>
+          <ListItemText primary="None Categories" />
+        </ListItem>
+      </div>
+    )
+  }
+}
 
 const styles = theme => ({
   nested: {
@@ -20,52 +76,8 @@ const styles = theme => ({
   }
 });
 
-class Categories extends Component {
-  static propTypes = {
-    classes: PropTypes.object.isRequired,
-    categories: PropTypes.array.isRequired,
-  }
-
-  state = { open: true }
-
-  handleClick = () => {
-    this.setState({ open: !this.state.open })
-  }
-
-  render() {
-    const { classes, categories } = this.props
-
-    return (
-      <div>
-        <ListItem button onClick={this.handleClick}>
-          <ListItemText primary="Categories" />
-          {this.state.open ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse component="li" in={this.state.open} timeout="auto" unmountOnExit>
-          <List disablePadding>
-            {categories.map((category, index) => (
-              // window.location.pathname === `/category/${category.path}` ?
-              window.location.pathname === `/${category.path}` ?
-                <ListItem button key={category.path} className={classes.nested}>
-                  <ListItemText primary={capitalizeFirst(category.name)} />
-                  {/* <ListItemText primary={capitalize(category.name)} /> */}
-                </ListItem>
-                :
-                // <ListItem button key={category.path} component={Link} to={`/category/${category.path}`} className={classes.nested}>
-                <ListItem button key={category.path} component={Link} to={`/${category.path}`} className={classes.nested}>
-                  <ListItemText primary={capitalizeFirst(category.name)} />
-                  {/* <ListItemText primary={capitalize(category.name)} /> */}
-                </ListItem>
-            ))}
-          </List>
-        </Collapse>
-      </div>
-    )
-  }
-}
-
-const mapStateToProps = ({ categories }) => ({
-  categories
+const mapStateToProps = (categories) => ({
+  ...categories
 })
 
 export default compose(
