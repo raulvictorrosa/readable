e15a778cb25f3ad40b89b5e541dd25f5645890c3
raulvictorrosa <raulvictorrosa@gmail.com>
2018-03-27 23:55:17 -0300

    feat: Add route to the list

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,30 +1,18 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { PropTypes } from 'prop-types'
-import compose from 'recompose/compose';
-import { withRouter, Switch, Route } from 'react-router-dom';
-import { getPosts } from '../actions/Posts'
-import { fetchPosts } from '../api/Posts'
-// import PostList from './post-list';
+// import { PropTypes } from 'prop-types'
+import { Switch, Route } from 'react-router-dom';
+import PostList from './PostList';
 // import PostForm from './post-form';
 // import PostDetail from './post-detail';
 // import { getSortedPostsWithSortedComments } from 'selectors';
 
 class Content extends Component {
-  componentDidMount() {
-    const { dispatch } = this.props
-    fetchPosts()
-      .then((posts) => dispatch(getPosts({ posts })))
-  }
-
   render() {
-    const { posts } = this.props;
-    console.log(posts)
     return (
       <div>
-        {/* <Switch> */}
+        <Switch>
           {/* <Route exact path='/' render={() => ( <PostList posts={posts} /> )} /> */}
-          {/* <Route exact path="/" component={PostList} /> */}
+          <Route exact path="/" component={PostList} />
 
           {/* <Route exact path='/posts/new' component={PostForm} /> */}
           {/* <Route path='/posts/:id/edit' render={({ match }) => (<PostForm initialValues={this.filterPostById(posts, match.params.id)} />)} />
@@ -44,14 +32,10 @@ class Content extends Component {
           {/* <Route path="/:category/:postId/edit" component={EditPost} /> */}
           {/* <Route path="/:category/:postId/comment" component={NewComment} /> */}
           {/* <Route path="/:category/:postId/:commentId/edit" component={EditComment} /> */}
-        {/* </Switch>  */}
+        </Switch>
       </div>
     )
   }
-};
+}
 
-const mapStateToProps = (post) => ({ post })
-
-export default connect(
-  mapStateToProps
-)(Content)
+export default Content
