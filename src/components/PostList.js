f668514ff90270d81636d3e66517088b2e82b336
raulvictorrosa <raulvictorrosa@gmail.com>
2018-03-27 23:56:01 -0300

    feat: Post List

---
+++ src/components/PostList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import compose from 'recompose/compose';
+import { withRouter } from 'react-router-dom';
+import { getPosts } from '../actions/Posts'
+import { fetchPosts } from '../api/Posts'
+import Grid from 'material-ui/Grid';
+
+import PostCard from './PostCard'
+// import SinglePost from '../post/SinglePost'
+
+
+class PostList extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props
+    fetchPosts().then((posts) => dispatch(getPosts(posts)))
+  }
+
+  render() {
+    const { posts } = this.props
+
+    return (
+      <Grid item xs={12}>
+        <Grid container spacing={24}>
+          {posts.map(post => (
+            <Grid key={post.id} item xs={4}>
+              <PostCard postInfo={post} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+const mapStateToProps = (posts) => ({
+  ...posts
+})
+
+export default compose(
+  withRouter,
+  connect(mapStateToProps),
+)(PostList)
