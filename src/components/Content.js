75625607864be7a072658e7018d82b8fb277b228
raulvictorrosa <raulvictorrosa@gmail.com>
2018-03-29 13:25:44 -0300

    feat: Route to list posts, add post and edit post

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import { PropTypes } from 'prop-types'
 import { Switch, Route } from 'react-router-dom';
 import PostList from './PostList';
-// import PostForm from './post-form';
+import PostForm from './PostForm';
 // import PostDetail from './post-detail';
 // import { getSortedPostsWithSortedComments } from 'selectors';
 
@@ -11,17 +11,20 @@ class Content extends Component {
     return (
       <div>
         <Switch>
-          {/* <Route exact path='/' render={() => ( <PostList posts={posts} /> )} /> */}
           <Route exact path="/" component={PostList} />
+          <Route exact path='/posts/new' component={PostForm} />
+          <Route path='/posts/:id/edit' component={PostForm} />
 
+          {/* <Route exact path='/' render={() => ( <PostList posts={posts} /> )} /> */}
           {/* <Route exact path='/posts/new' component={PostForm} /> */}
-          {/* <Route path='/posts/:id/edit' render={({ match }) => (<PostForm initialValues={this.filterPostById(posts, match.params.id)} />)} />
-          <Route exact path='/posts/:id' render={({ match }) => (<PostDetail {...this.filterPostById(posts, match.params.id) } />)} />
-          <Route exact path='/categories/:name' render={({ match }) => (<PostList posts={this.filterPostByCategory(posts, match.params.name)} />)} /> */}
+          {/* <Route path='/posts/:id/edit' render={({ match }) => (<PostForm initialValues={this.filterPostById(posts, match.params.id)} />)} /> */}
+          {/* <Route exact path='/posts/:id' render={({ match }) => (<PostDetail {...this.filterPostById(posts, match.params.id) } />)} /> */}
+          {/* <Route exact path='/categories/:name' render={({ match }) => (<PostList posts={this.filterPostByCategory(posts, match.params.name)} />)} /> */}
         {/* </Switch> */}
 
         {/* <Route exact path='/:category' component={Category} />
         <Route path='/:category/:post_id' component={PostDetail} /> */}
+        </Switch>
 
         {/* <Switch>   */}
           {/* <Route exact path="/" component={Home} /> */}
@@ -32,7 +35,7 @@ class Content extends Component {
           {/* <Route path="/:category/:postId/edit" component={EditPost} /> */}
           {/* <Route path="/:category/:postId/comment" component={NewComment} /> */}
           {/* <Route path="/:category/:postId/:commentId/edit" component={EditComment} /> */}
-        </Switch>
+        {/* </Switch> */}
       </div>
     )
   }
