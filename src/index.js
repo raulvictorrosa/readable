34952db60c08851369d13d0da8f9c31349a816cb
raulvictorrosa <raulvictorrosa@gmail.com>
2018-03-30 19:44:21 -0300

    feat: Add middleware logger and crash reporter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Raven from 'raven-js'
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux'
@@ -11,20 +12,44 @@ import 'normalize.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-// const logger = store => next => action => {
-//   console.group(action.type)
-//   console.info('dispatching', action)
-//   let result = next(action)
-//   console.log('next state', store.getState())
-//   console.groupEnd(action.type)
-//   return result
-// }
+/**
+ * @description Logs all actions and states after they are dispatched.
+ */
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+/**
+ * @description Sends crash reports as state is updated and listeners are notified.
+ */
+const crashReporter = store => next => action => {
+  try {
+    return next(action)
+  } catch (err) {
+    console.error('Caught an exception!', err)
+    Raven.captureException(err, {
+      extra: {
+        action,
+        state: store.getState()
+      }
+    })
+    throw err
+  }
+}
 
 const store = createStore(
   Reducers,
   composeEnhancers(
-    applyMiddleware(Thunk)
-    // applyMiddleware(logger, Thunk)
+    applyMiddleware(
+      Thunk,
+      logger,
+      crashReporter,
+    )
   )
 )
 
