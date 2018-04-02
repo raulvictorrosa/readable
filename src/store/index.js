30dc35ff21107295e027bbd5abf3e543efc78c7f
raulvictorrosa <raulvictorrosa@gmail.com>
2018-04-02 10:06:43 -0300

    feat: Comment crash reporter

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger'
-import Raven from 'raven-js'
+// import Raven from 'raven-js'
 import Thunk from 'redux-thunk'
 import Reducers from '../reducers'
 
@@ -14,20 +14,20 @@ const logger = createLogger()
 /**
  * @description Sends crash reports as state is updated and listeners are notified.
  */
-const crashReporter = store => next => action => {
-  try {
-    return next(action)
-  } catch (err) {
-    console.error('Caught an exception!', err)
-    Raven.captureException(err, {
-      extra: {
-        action,
-        state: store.getState()
-      }
-    })
-    throw err
-  }
-}
+// const crashReporter = store => next => action => {
+//   try {
+//     return next(action)
+//   } catch (err) {
+//     console.error('Caught an exception!', err)
+//     Raven.captureException(err, {
+//       extra: {
+//         action,
+//         state: store.getState()
+//       }
+//     })
+//     throw err
+//   }
+// }
 
 const store = createStore(
   Reducers,
