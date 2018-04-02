97d3d2b77ea8fb15daa68f0a66e6ac5eebb27fb5
raulvictorrosa <raulvictorrosa@gmail.com>
2018-04-02 09:59:40 -0300

    feat: Enhance post actions to call post detail

--- src/components/PostCardActions.js
+++ src/components/PostCardActions.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
+import compose from 'recompose/compose';
 import { connect } from 'react-redux'
 import { votePostCount } from '../actions/Posts';
-import { withRouter } from 'react-router-dom'
-import compose from 'recompose/compose';
+import { Link, withRouter } from 'react-router-dom'
 import { withStyles } from 'material-ui/styles';
 import { CardActions } from 'material-ui/Card';
 import IconButton from 'material-ui/IconButton';
@@ -17,37 +17,36 @@ import ForwardIcon from 'material-ui-icons/Forward';
 class PostCardActions extends Component {
   static propTypes = {
     classes: PropTypes.object.isRequired,
-    postInfo: PropTypes.object.isRequired
+    id: PropTypes.string.isRequired,
+    commentCount: PropTypes.number.isRequired,
+    voteScore: PropTypes.number.isRequired,
+    voteUp: PropTypes.func.isRequired,
+    voteDown: PropTypes.func.isRequired,
   }
 
-  // componentDidMount() {
-  //   const { dispatch, postInfo } = this.props
-  //   const { id } = postInfo
-  //   fetchPostById(id).then((posts) => dispatch(getPosts(posts)))
-  // }
-
   handleLike = () => {
-    const { voteUp, postInfo } = this.props;
-    const { id } = postInfo
+    const { id, voteUp } = this.props;
     voteUp(id)
   }
 
   handleDislike = () => {
-    const { voteDown, postInfo } = this.props;
-    const { id } = postInfo
+    const { id, voteDown } = this.props;
     voteDown(id)
   }
 
   handleInsertComment = () => {
-    const { /* dispatch,  */postInfo } = this.props;
-    const { id } = postInfo
-    // fetchPosts().then((posts) => dispatch(getPosts(posts)))
+    const { id/* , insertComment */ } = this.props;
     console.log(`Insert comment on post ${id}`);
   }
 
   render() {
-    const { classes, postInfo } = this.props;
-    const { commentCount, voteScore } = postInfo;
+    const {
+      classes,
+      id,
+      commentCount,
+      voteScore
+    } = this.props;
+    console.log(this.props)
     return (
       <div>
         <CardActions disableActionSpacing>
@@ -82,13 +81,13 @@ class PostCardActions extends Component {
 
           <div className={classes.flexGrow} />
 
-          <IconButton
-            aria-label="Show more"
-            onClick={this.handleExpandClick}
-          >
-            {/* <Link to="/search">Add a book</Link> */}
-            <ForwardIcon />
-          </IconButton>
+          <Link to={`/posts/${id}`}>
+            <IconButton
+              aria-label="Show more"
+            >
+              <ForwardIcon />
+            </IconButton>
+          </Link>
         </CardActions>
       </div>
     )
@@ -97,21 +96,20 @@ class PostCardActions extends Component {
 
 const styles = theme => ({
   flexGrow: {
-    flex: '1 1 auto',
+    flex: '1 1 auto'
   }
 })
 
-// const mapStateToProps = (posts) => ({
-//   ...posts
-// })
-
 const mapDispatchToProps = dispatch => ({
   voteUp: id => dispatch(votePostCount(id, 'upVote')),
-  voteDown: id => dispatch(votePostCount(id, 'downVote')),
+  voteDown: id => dispatch(votePostCount(id, 'downVote'))
 })
 
 export default compose(
   withRouter,
   withStyles(styles),
-  connect(null, mapDispatchToProps),
+  connect(
+    null,
+    mapDispatchToProps
+  )
 )(PostCardActions)
