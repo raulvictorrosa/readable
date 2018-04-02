764d2fd8a61973c664543566a77508818e98a852
raulvictorrosa <raulvictorrosa@gmail.com>
2018-04-02 09:35:06 -0300

    feat: Enhance the store

--- src/index.js
+++ src/index.js
@@ -1,64 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import Raven from 'raven-js'
-import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux'
-import Thunk from 'redux-thunk'
-import Reducers from './reducers'
-import registerServiceWorker from './registerServiceWorker';
+import store from './store'
+import { BrowserRouter } from 'react-router-dom';
 import Index from './components';
-import 'normalize.css';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-/**
- * @description Logs all actions and states after they are dispatched.
- */
-const logger = store => next => action => {
-  console.group(action.type)
-  console.info('dispatching', action)
-  let result = next(action)
-  console.log('next state', store.getState())
-  console.groupEnd(action.type)
-  return result
-}
-
-/**
- * @description Sends crash reports as state is updated and listeners are notified.
- */
-const crashReporter = store => next => action => {
-  try {
-    return next(action)
-  } catch (err) {
-    console.error('Caught an exception!', err)
-    Raven.captureException(err, {
-      extra: {
-        action,
-        state: store.getState()
-      }
-    })
-    throw err
-  }
-}
-
-const store = createStore(
-  Reducers,
-  composeEnhancers(
-    applyMiddleware(
-      Thunk,
-      logger,
-      crashReporter,
-    )
-  )
-)
+import 'typeface-roboto'
+import CssBaseline from 'material-ui/CssBaseline';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <Index />
+      <Fragment>
+        <CssBaseline />
+        <Index />
+      </Fragment>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
-);
-registerServiceWorker();
+)
+registerServiceWorker()
---
+++ src/store/index.js
@@ -0,0 +1,43 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger'
+import Raven from 'raven-js'
+import Thunk from 'redux-thunk'
+import Reducers from '../reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+/**
+ * @description Logs all actions and states after they are dispatched.
+ */
+const logger = createLogger()
+
+/**
+ * @description Sends crash reports as state is updated and listeners are notified.
+ */
+const crashReporter = store => next => action => {
+  try {
+    return next(action)
+  } catch (err) {
+    console.error('Caught an exception!', err)
+    Raven.captureException(err, {
+      extra: {
+        action,
+        state: store.getState()
+      }
+    })
+    throw err
+  }
+}
+
+const store = createStore(
+  Reducers,
+  composeEnhancers(
+    applyMiddleware(
+      Thunk,
+      logger,
+      // crashReporter,
+    )
+  )
+)
+
+export default store
