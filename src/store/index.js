c70f2b55ad871eef2838a1ec64dd44dc67a14e17
raulvictorrosa <raulvictorrosa@gmail.com>
2018-04-06 08:10:59 -0300

    feat: Enhance the redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger'
-// import Raven from 'raven-js'
 import Thunk from 'redux-thunk'
 import Reducers from '../reducers'
 
@@ -11,31 +10,12 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
  */
 const logger = createLogger()
 
-/**
- * @description Sends crash reports as state is updated and listeners are notified.
- */
-// const crashReporter = store => next => action => {
-//   try {
-//     return next(action)
-//   } catch (err) {
-//     console.error('Caught an exception!', err)
-//     Raven.captureException(err, {
-//       extra: {
-//         action,
-//         state: store.getState()
-//       }
-//     })
-//     throw err
-//   }
-// }
-
 const store = createStore(
   Reducers,
   composeEnhancers(
     applyMiddleware(
       Thunk,
-      logger,
-      // crashReporter,
+      logger
     )
   )
 )
