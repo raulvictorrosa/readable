f45bef0dd8bd7017af58d952ac1f267ce0b0cfb0
raulvictorrosa <raulvictorrosa@gmail.com>
2018-04-06 08:04:55 -0300

    feat: Change to stateless function

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,44 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import { PropTypes } from 'prop-types'
 import { Switch, Route } from 'react-router-dom';
 import PostList from './PostList';
 import PostForm from './PostForm';
-// import PostDetail from './post-detail';
+import PostDetail from './PostDetail';
 // import { getSortedPostsWithSortedComments } from 'selectors';
 
-class Content extends Component {
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route exact path="/" component={PostList} />
-          <Route exact path='/posts/new' component={PostForm} />
-          <Route path='/posts/:id/edit' component={PostForm} />
+const Content = () =>
+  <Switch>
+    <Route exact path="/" component={PostList} />
+    <Route exact path='/posts/new' component={PostForm} />
+    <Route path='/posts/:id/edit' component={PostForm} />
+    <Route exact path='/posts/:id' component={PostDetail} />
+    {/* <Route exact path='/posts/:id' render={({ match }) => (<PostDetail {...this.filterPostById(posts, match.params.id) } />)} /> */}
 
-          {/* <Route exact path='/' render={() => ( <PostList posts={posts} /> )} /> */}
-          {/* <Route exact path='/posts/new' component={PostForm} /> */}
-          {/* <Route path='/posts/:id/edit' render={({ match }) => (<PostForm initialValues={this.filterPostById(posts, match.params.id)} />)} /> */}
-          {/* <Route exact path='/posts/:id' render={({ match }) => (<PostDetail {...this.filterPostById(posts, match.params.id) } />)} /> */}
-          {/* <Route exact path='/categories/:name' render={({ match }) => (<PostList posts={this.filterPostByCategory(posts, match.params.name)} />)} /> */}
-        {/* </Switch> */}
+    {/* <Route exact path='/' render={() => ( <PostList posts={posts} /> )} /> */}
+    {/* <Route exact path='/posts/new' component={PostForm} /> */}
+    {/* <Route path='/posts/:id/edit' render={({ match }) => (<PostForm initialValues={this.filterPostById(posts, match.params.id)} />)} /> */}
+    {/* <Route exact path='/categories/:name' render={({ match }) => (<PostList posts={this.filterPostByCategory(posts, match.params.name)} />)} /> */}
+  {/* </Switch> */}
 
-        {/* <Route exact path='/:category' component={Category} />
-        <Route path='/:category/:post_id' component={PostDetail} /> */}
-        </Switch>
+  {/* <Route exact path='/:category' component={Category} />
+  <Route path='/:category/:post_id' component={PostDetail} /> */}
 
-        {/* <Switch>   */}
-          {/* <Route exact path="/" component={Home} /> */}
-          {/* <Route exact path="/post-new" component={PostNew} /> */}
-          {/* <Route exact path="/:category" component={Category} /> */}
-          {/* <Route exact path="/:category" component={Home} /> */}
-          {/* <Route exact path="/:category/:postId" component={PostDetail} /> */}
-          {/* <Route path="/:category/:postId/edit" component={EditPost} /> */}
-          {/* <Route path="/:category/:postId/comment" component={NewComment} /> */}
-          {/* <Route path="/:category/:postId/:commentId/edit" component={EditComment} /> */}
-        {/* </Switch> */}
-      </div>
-    )
-  }
-}
+  {/* <Switch>   */}
+    {/* <Route exact path="/" component={Home} /> */}
+    {/* <Route exact path="/post-new" component={PostNew} /> */}
+    {/* <Route exact path="/:category" component={Category} /> */}
+    {/* <Route exact path="/:category" component={Home} /> */}
+    {/* <Route exact path="/:category/:postId" component={PostDetail} /> */}
+    {/* <Route path="/:category/:postId/edit" component={EditPost} /> */}
+    {/* <Route path="/:category/:postId/comment" component={NewComment} /> */}
+    {/* <Route path="/:category/:postId/:commentId/edit" component={EditComment} /> */}
+  {/* </Switch> */}
+  </Switch>
 
 export default Content
