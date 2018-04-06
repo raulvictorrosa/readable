5a5fc0e31afb91dc912e7380cd2de9886e13f806
raulvictorrosa <raulvictorrosa@gmail.com>
2018-04-06 08:07:47 -0300

    feat: Enhance component post card actions

--- src/components/PostCardActions.js
+++ src/components/PostCardActions.js
@@ -4,49 +4,57 @@ import compose from 'recompose/compose';
 import { connect } from 'react-redux'
 import { votePostCount } from '../actions/Posts';
 import { Link, withRouter } from 'react-router-dom'
+import Moment from 'moment'
 import { withStyles } from 'material-ui/styles';
-import { CardActions } from 'material-ui/Card';
+import Card, { CardHeader, CardContent, CardActions } from 'material-ui/Card';
+import blueGrey from 'material-ui/colors/blueGrey';
+import Collapse from 'material-ui/transitions/Collapse';
 import IconButton from 'material-ui/IconButton';
 import Typography from 'material-ui/Typography';
 import ThumbUpIcon from 'material-ui-icons/ThumbUp';
 import ThumbDownIcon from 'material-ui-icons/ThumbDown';
 import InsertCommentIcon from 'material-ui-icons/InsertComment';
-// import ModeCommentIcon from 'material-ui-icons/ModeComment';
+import ModeCommentIcon from 'material-ui-icons/ModeComment';
 import ForwardIcon from 'material-ui-icons/Forward';
+// import CommentList from './CommentList';
 
 class PostCardActions extends Component {
   static propTypes = {
     classes: PropTypes.object.isRequired,
-    id: PropTypes.string.isRequired,
-    commentCount: PropTypes.number.isRequired,
-    voteScore: PropTypes.number.isRequired,
     voteUp: PropTypes.func.isRequired,
-    voteDown: PropTypes.func.isRequired,
+    voteDown: PropTypes.func.isRequired
+  }
+
+  state = {
+    expanded: false
   }
 
   handleLike = () => {
-    const { id, voteUp } = this.props;
+    const { post, voteUp } = this.props
+    const { id } = post
     voteUp(id)
   }
 
   handleDislike = () => {
-    const { id, voteDown } = this.props;
+    const { post, voteDown } = this.props
+    const { id } = post
     voteDown(id)
   }
 
   handleInsertComment = () => {
-    const { id/* , insertComment */ } = this.props;
-    console.log(`Insert comment on post ${id}`);
+    if (this.props.match.path === '/posts/:id') {
+      this.setState({ expanded: !this.state.expanded })
+
+      const { post/* , insertComment */ } = this.props
+      const { id/* , insertComment */ } = post
+      console.log(`Insert comment on post ${id}`)
+    }
   }
 
   render() {
-    const {
-      classes,
-      id,
-      commentCount,
-      voteScore
-    } = this.props;
-    console.log(this.props)
+    const { classes, post, match } = this.props
+    const { id, author, commentCount, timestamp, title, voteScore } = post
+    const { expanded } = this.state
     return (
       <div>
         <CardActions disableActionSpacing>
@@ -70,9 +78,13 @@ class PostCardActions extends Component {
 
           <IconButton
             aria-label="Insert Comment"
+            aria-expanded={this.state.expanded}
             onClick={this.handleInsertComment}
           >
-            <InsertCommentIcon />
+            {expanded
+              ? <ModeCommentIcon />
+              : <InsertCommentIcon />
+            }
           </IconButton>
 
           <Typography color="secondary" type="body2">
@@ -80,15 +92,20 @@ class PostCardActions extends Component {
           </Typography>
 
           <div className={classes.flexGrow} />
-
-          <Link to={`/posts/${id}`}>
-            <IconButton
-              aria-label="Show more"
-            >
-              <ForwardIcon />
-            </IconButton>
-          </Link>
+          {match.path !== '/posts/:id' &&
+            <Link to={`/posts/${id}`}>
+              <IconButton
+                aria-label="Show more"
+                >
+                <ForwardIcon />
+              </IconButton>
+            </Link>
+          }
         </CardActions>
+
+        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+          {/* <Comments id={id}/> */}
+        </Collapse>
       </div>
     )
   }
