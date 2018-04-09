133f34764def11092ec83a857fd9df7f69b27116
raulvictorrosa <raulvictorrosa@gmail.com>
2018-04-09 11:59:31 -0300

    feat: Reducer to fetch comments

--- src/reducers/Comments.js
+++ src/reducers/Comments.js
@@ -1,13 +1,20 @@
-import { ADD_COMMENT, DELETE_COMMENT, /* FETCH_COMMENT, */ /* VOTE_COMMENT, */ EDIT_COMMENT, FETCH_COMMENTS } from '../actions/constants';
+import {
+  /* ADD_COMMENT, */
+  /* DELETE_COMMENT, */
+  /* FETCH_COMMENT, */
+  /* VOTE_COMMENT, */
+  /* EDIT_COMMENT, */
+  FETCH_COMMENTS
+} from '../actions/Constants';
 
-function comments(state = {}, action) {
-  const { comments, commentId, parentId, updatedComment } = action
+const comments = (state = [], action) => {
+  // const { comments, commentId, parentId, updatedComment } = action
   switch (action.type) {
     case FETCH_COMMENTS:
-      return Object.assign({}, state, { [parentId]: comments })
+      return [ ...action.comments ]
 
-    case ADD_COMMENT:
-      return Object.assign({}, state, { [parentId]: comments })
+    // case ADD_COMMENT:
+    //   return Object.assign({}, state, { [parentId]: comments })
 
     //TODO: Add reducer to FETCH_COMMENT
 
@@ -22,19 +29,19 @@ function comments(state = {}, action) {
     //     })
     //   }
 
-    case EDIT_COMMENT:
-      return {
-        ...state,
-        [parentId]: state[parentId].map(comment => {
-          if (comment.id === commentId) {
-            comment = updatedComment
-          }
-          return comment
-        })
-      }
+    // case EDIT_COMMENT:
+    //   return {
+    //     ...state,
+    //     [parentId]: state[parentId].map(comment => {
+    //       if (comment.id === commentId) {
+    //         comment = updatedComment
+    //       }
+    //       return comment
+    //     })
+    //   }
 
-    case DELETE_COMMENT:
-      return state
+    // case DELETE_COMMENT:
+    //   return state
 
     default:
       return state
