bbcfdec21aa7500aac1b744fe5b259acb3a968a5
raulvictorrosa <raulvictorrosa@gmail.com>
2018-04-09 11:56:14 -0300

    feat: Organize imports and list comments

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-// import { PropTypes } from 'prop-types';
-import compose from 'recompose/compose';
-import { connect } from 'react-redux'
-import { getComments } from '../actions/Posts';
-import Moment from 'moment';
-import { withStyles } from 'material-ui/styles';
-import Card, { CardHeader, CardContent, CardActions } from 'material-ui/Card';
 import Avatar from 'material-ui/Avatar';
+import Card, { CardContent, CardHeader } from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import blueGrey from 'material-ui/colors/blueGrey';
+import { withStyles } from 'material-ui/styles';
+import Moment from 'moment';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { PropTypes } from 'prop-types';
+import compose from 'recompose/compose';
+import { getComments } from '../actions/Comments';
+import CommentOptions from './CommentOptions';
 
 class CommentList extends Component {
   componentDidMount() {
@@ -17,30 +18,36 @@ class CommentList extends Component {
   }
 
   render() {
+    const { classes, comments } = this.props
     return (
-      <Card>
-        {/* <CardHeader
-          avatar={
-            <Avatar
-              aria-label={author}
-              alt={author}
-              className={classes.avatar}
-            >
-              {author ? author.substr(0, 2) : ''}
-            </Avatar>
-          }
-          // action={<CommentOptions postId={id} />}
-          title={title}
-          subheader={Moment(timestamp).format('MMMM DD, YYYY')}
-        />
+      <div>
+        <Card>
+          {comments.map(comment => (
+            <div key={comment.id}>
+              <CardHeader
+                avatar={
+                  <Avatar
+                    aria-label={comment.author}
+                    alt={comment.author}
+                    className={classes.avatar}
+                  >
+                    {comment.author ? comment.author.substr(0, 2) : ''}
+                  </Avatar>
+                }
+                action={<CommentOptions id={comment.id} />}
+                title={comment.title}
+                subheader={Moment(comment.timestamp).format('MMMM DD, YYYY')}
+              />
 
-        <CardContent>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-        </CardContent> */}
-      </Card>
+              <CardContent>
+                <Typography paragraph>
+                  {comment.body}
+                </Typography>
+              </CardContent>
+            </div>
+          ))}
+        </Card>
+      </div>
     )
   }
 }
