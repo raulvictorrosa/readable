d91041f0303e8bad355bc087d87ceaf9d4f6dcab
raulvictorrosa <raulvictorrosa@gmail.com>
2018-04-09 12:00:15 -0300

    feat: import reducer to posts ans comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
 import { combineReducers } from 'redux'
 // import { reducer as form } from 'redux-form';
 
-import posts from './Posts'
+import { posts, post } from './Posts'
 import categories from './Categories'
-// import comments from './comments'
+import comments from './Comments'
 
 export default combineReducers({
   posts,
+  post,
   categories,
-  // comments
+  comments
 })
